a59569699107a2f17acef748011b0dae97e81934
biszczak <dbiszczak@gmail.com>
2019-07-19 10:52:43 +0200

    Updates

---
+++ precache-manifest.a00712550b3f770d1e1045271f3104d7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06354758d704fa383ddd21eb69f006b",
+    "url": "/bikeshop/index.html"
+  },
+  {
+    "revision": "bea0c2acf69396a06de7",
+    "url": "/bikeshop/static/css/main.55bbf5cb.chunk.css"
+  },
+  {
+    "revision": "84384715ffe640966cfd",
+    "url": "/bikeshop/static/js/2.0880acbd.chunk.js"
+  },
+  {
+    "revision": "bea0c2acf69396a06de7",
+    "url": "/bikeshop/static/js/main.5bbc03b7.chunk.js"
+  },
+  {
+    "revision": "6912c3c43fcf9e6846e0",
+    "url": "/bikeshop/static/js/runtime~main.ef263cd9.js"
+  },
+  {
+    "revision": "43886ce16d67666470efd933ddb37416",
+    "url": "/bikeshop/static/media/Milkshake.43886ce1.woff"
+  },
+  {
+    "revision": "58e8b90f791269ee1c6820c063df2127",
+    "url": "/bikeshop/static/media/Milkshake.58e8b90f.woff2"
+  },
+  {
+    "revision": "959463ba552f50a450acc23af0a23e80",
+    "url": "/bikeshop/static/media/Milkshake.959463ba.eot"
+  },
+  {
+    "revision": "d96003978f2f5517beae78ba472cd938",
+    "url": "/bikeshop/static/media/Milkshake.d9600397.svg"
+  },
+  {
+    "revision": "3fbfc4ea313c78c46ccfe1c8730aa02a",
+    "url": "/bikeshop/static/media/background1.3fbfc4ea.jpg"
+  },
+  {
+    "revision": "fca664c0f9d9880cda417358e01b80d7",
+    "url": "/bikeshop/static/media/bibycle.fca664c0.jpg"
+  },
+  {
+    "revision": "1fd615965e13d3bcdf90c1fa63de5e98",
+    "url": "/bikeshop/static/media/bicycle1.1fd61596.png"
+  },
+  {
+    "revision": "108e09bdcbfb12c67260533b03ab17e0",
+    "url": "/bikeshop/static/media/bicycle2.108e09bd.png"
+  },
+  {
+    "revision": "77fe5b11c3e021bb1ed59a2a3cf19ac2",
+    "url": "/bikeshop/static/media/bicycle3.77fe5b11.png"
+  },
+  {
+    "revision": "b6c24a300c48a1940e36d359559e9e4d",
+    "url": "/bikeshop/static/media/bicycle4.b6c24a30.png"
+  }
+]);
